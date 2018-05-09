8c9684b59f29bf517c38facec4e7a314c2c2b969
rr999888tw <rr999888tw@gmail.com>
2018-05-09 19:56:11 +0800

    ohya

--- back-end/bin/ver1.js
+++ back-end/bin/ver1.js
@@ -1,50 +1,17 @@
 var express = require('express')
-    , bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 // var app = express();
 var router = express.Router()
 
-router.use(bodyParser.json());
+// router.use('/', ()=> {console.log("2!!!")})
+// router.use(bodyParser.json());
 
-
-const a = {
-  title: "The Basics - Networking",
-  description: "Your app fetched this from a remote endpoint!",
-  movies: [
-    { title: "Star Wars", releaseYear: "1977"},
-    { title: "Back to the Future", releaseYear: "1985"},
-    { title: "The Matrix", releaseYear: "1999"},
-    { title: "Inception", releaseYear: "2010"},
-    { title: "Interstellar", releaseYear: "2014"}
-  ]
-}
-
-router.use('/users', function (request, response) {
-    // console.log(request.headers);      // your JSON
-    
+router.use('/', function (request, response) {
+    console.log(request.body);      // your JSON
+    console.log("!!!!!!!!!!!")
     response.setHeader("Access-Control-Allow-Origin", '*');
-
-    response.json(a);    // echo the result back
+    response.json({text: "hello"});    // echo the result back
 });
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-})
-// define the home page route
-router.get('/', function (req, res, next) {
-    // res.send('Birds home page')
-    console.log('middleware 1');
-    next();
-})
-// define the about route
-router.get('/about', function (req, res, next) {
-    res.send('About birds');
-})
-
 module.exports = router;
-
-
-
-// app.listen(3000);
--- back-end/bin/ver2.js
+++ back-end/bin/ver2.js
@@ -1,8 +1,21 @@
-var birds = require('./ver1');
+var router = require('./ver1');
 var express = require('express');
 var app = express();
 
 // ...
 
-app.use('/birds', birds);
-app.listen(3001);
+// app.use('/birds', birds);
+
+
+// app.use(bodyParser.json());
+// app.use('/users', function (request, response) {
+//     // console.log(request.headers);      // your JSON
+
+//     response.setHeader("Access-Control-Allow-Origin", '*');
+//     response.json({ text: "hello" });    // echo the result back
+// });
+
+// app.use('/users', () => { console.log("!!!1") })
+app.use('/users', router)
+
+app.listen(4500);
